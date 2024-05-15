54a4afa40d374a739152fb5625a711577fab3356
Nix <130068039+covennix@users.noreply.github.com>
2024-05-15 11:56:54 -0300

    scrr

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
+const input = document.getElementById('phone');
+const gerarLink = document.querySelectorAll("[link]");
+const gerarCopiar = document.getElementById("[copiar]")
+
 // Formatar o telefone
-document.getElementById("phone").addEventListener("input", function(event) {
-    var phoneInput = event.target;
-    var phone = phoneInput.value.replace(/\D/g, ''); 
-    var formattedPhone = '';
-    if (phone.length > 0) {
-        formattedPhone = '(' + phone.substring(0, 2);
-        if (phone.length > 2) {
-            formattedPhone += ') ';
-            if (phone.length > 7) {
-                formattedPhone += phone.substring(2, 7) + '-' + phone.substring(7, 11);
-            } else {
-                formattedPhone += phone.substring(2, phone.length);
-            }
-        }
-    }
-    phoneInput.value = formattedPhone;
-    validarTelefone(phone);
-});
+document.getElementById('input').addEventListener('input', function(e) {
+ 
+    let inputValue = e.target.value;
+    
+    let cleanedValue = inputValue.replace(/\D/g, '');
+    
+    let formattedValue = cleanedValue.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    
+    e.target.value = formattedValue;
 
-
-// Validar telefone
-function validarTelefone(numeroTelefone) {
-    var botaoGerarCopiar = document.getElementById("gerarCopiar");
-    if (numeroTelefone.length === 11) {
-        botaoGerarCopiar.disabled = false;
-        document.getElementById("phone").style.Color = "blue"; 
+    // Verifica se o telefone é válido (completo)
+    if (cleanedValue.length === 11) {
+        e.target.style.color = "green"; 
     } else {
-        botaoGerarCopiar.disabled = true;
-        document.getElementById("phone").style.Color = "initial"; 
+        e.target.style.color = ""; 
     }
-}
 
-// Gerar e copiar link
-document.getElementById("gerarCopiar").addEventListener("click", function() {
-    var phone = document.getElementById("phone").value.replace(/\D/g, ''); 
-    var link = gerarLink(phone);
-    copiarParaAreaDeTransferencia(link);
-});
-
-// Gerar link do WhatsApp
-function gerarLink(numeroTelefone) {
-    var base_url = "https://wa.me/55";
-    var url = base_url + numeroTelefone;
-    return url;
-}
-
-// Copiar link para área de transferência
-function copiarParaAreaDeTransferencia(texto) {
-    var inputTemporario = document.createElement("input");
-    inputTemporario.value = texto;
-    document.body.appendChild(inputTemporario);
-    inputTemporario.select();
-    document.execCommand("copy");
-    document.body.removeChild(inputTemporario);
-    alert("Link copiado para área de transferência!");
-}
-
+ });
 
